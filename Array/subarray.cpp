debf95bfb4087ea9a92baefd59e8680bdb312d69
ritik chaurasia <ritikchaurasia0707@gmailcom>
2024-09-21 12:31:35 +0530

    subarray Sum using Kadan's algo

--- Array/subarray.cpp
+++ Array/subarray.cpp
@@ -24,6 +24,31 @@
 // }
 
 // =================== Maximum subarray sum ==================================
+// we have input for understood   {3 , -4 , 5 , 4 , -1 , 7 , -8}
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int n;
+//     cout<<"Enter the array size: ";
+//     cin>>n;
+//     int arr[n];
+//     for(int i=0;i<n;i++){
+//         cin>>arr[i];
+//     } 
+//     int maxSum = n-1;
+//     for(int str=0;str<n;str++){
+//         int curSum = 0;
+//         for(int end=str;end<n;end++){
+//             curSum = curSum + arr[end]; 
+//             maxSum = max(curSum , maxSum);
+//         }
+//     }
+//     cout<<"Max subarray sum = "<<maxSum<<endl;
+//     return 0;
+// }
+
+// ============================== Kadan's algorithm =============================
 
 #include<iostream>
 using namespace std;
@@ -34,15 +59,15 @@ int main(){
     int arr[n];
     for(int i=0;i<n;i++){
         cin>>arr[i];
-    }
-    int maxSum = n-1;
-    for(int str=0;str<n;str++){
-        int curSum = 0;
-        for(int end=str;end<n;end++){
-            curSum = curSum + arr[end];
-            maxSum = max(curSum , maxSum);
+    } 
+    int curSum = 0 , maxSum = n-1;
+    for(int i = 0 ; i<n;i++){
+        curSum = curSum + arr[i];
+        maxSum = max(curSum , maxSum);
+        if(curSum < 0){
+            curSum = 0;
         }
     }
-    cout<<"Max subarray sum = "<<maxSum<<endl;
+    cout<<"maximum subarray sum is :"<<maxSum;
     return 0;
 }
