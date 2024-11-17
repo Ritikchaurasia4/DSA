9bf679b7d640d2d415a073e68cb78162a1630d61
ritik chaurasia <ritikchaurasia0707@gmailcom>
2024-11-17 17:30:15 +0530

    linkedlist , insertion deletion and searching

--- LinkedList/node.cpp
+++ LinkedList/node.cpp
@@ -62,10 +62,10 @@ void deletion(node* &head , int val){
     while(temp->next->data != val){
         temp = temp->next;
     }
-    node* todelete = temp->next;
+    node* toDelete = temp->next;
     temp->next = temp->next->next;
 
-    delete todelete;
+    delete toDelete;
 }
 
 int main(){
