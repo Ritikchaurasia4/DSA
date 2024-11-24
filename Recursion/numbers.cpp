81807b7535f18810ea45dc51ae42e85f55e1378f
ritik chaurasia <ritikchaurasia0707@gmailcom>
2024-11-24 16:48:02 +0530

    print Number using recursion

---
+++ Recursion/numbers.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+void printNum(int n){
+    if(n == 1){     // Base case
+        cout<<"1\n";
+        return ;
+    }
+    cout<<n<<" ";
+    printNum(n-1);
+}
+int main(){
+    printNum(4);
+    return 0;
+}
